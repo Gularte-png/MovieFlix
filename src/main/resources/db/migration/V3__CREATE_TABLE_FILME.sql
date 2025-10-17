3e0572ff12c795a86ca0667f62ec5552eb2aad7b
André <andresacon2@gmail.com>
2025-10-16 23:13:53 -0300

    feat: adiciona migrations de filme (V3,V4,V5)

---
+++ src/main/resources/db/migration/V3__CREATE_TABLE_FILME.sql
@@ -0,0 +1,10 @@
+CREATE TABLE filme
+(
+    id                 serial PRIMARY KEY,
+    titulo             VARCHAR(255)       NOT NULL,
+    descricao          text,
+    data_de_lançamento DATE,
+    avaliacao          NUMERIC,
+    criado             TIMESTAMP,
+    atualizado         TIMESTAMP
+);
